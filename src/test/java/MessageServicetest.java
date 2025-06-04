6e6e684f65647b8c5b35e3209f05eb05dfaefdbd
lucasG-btfan <lucas.gordillo.3591@gmail.com>
2025-06-04 12:37:06 -0300

    Ahora funciona profe

--- src/test/java/MessageServicetest.java
+++ src/test/java/MessageServicetest.java
@@ -1,16 +1,7 @@
 import org.example.model.Message;
 import org.example.repository.MessageRepository;
 import org.example.service.MessageService;
-import org.mockito.Mock;
-
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-
-import org.junit.jupiter.api.BeforeEach;
-
 import org.junit.jupiter.api.Test;
-
-import java.awt.*;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,36 +9,24 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class MessageServicetest {
-    @Mock
-        private MessageService messageService;
-        MessageService mockmessage=mock(MessageService.class);
-        Message mensaje=new Message(423L,"Hola");
-        // crear objeto Mensaje sin definir
+    @Test
+    public void savetest(){
+        MessageRepository mockmessage=mock(MessageRepository.class);
+        Message mensaje = new Message(423L, "Hola");
+        when(mockmessage.save(mensaje)).thenReturn(mensaje);
 
-        Optional op;
-
-        @BeforeEach
-        void setUp(){
-            MockitoAnnotations.openMocks(this);
-            MessageService mockmessage=mock(MessageService.class);
-        }
-        @Test
-        public void savetest(){
-            MessageRepository mockmessage=mock(MessageRepository.class);
-            when(mockmessage.save(mensaje)).thenReturn(mensaje);
-
-            MessageService messageService= new MessageService(mockmessage);
-            Message resultadomensaje=messageService.createMessage(mensaje);
-            assertEquals(resultadomensaje,mensaje);
-        }
+        MessageService messageService = new MessageService(mockmessage);
+        Message resultadoMensaje = messageService.createMessage(mensaje);
+        assertEquals(mensaje, resultadoMensaje);
+    }
     @Test
     public void getMessagebyIdTest(){
         MessageRepository mockmessage=mock(MessageRepository.class);
-        when(mockmessage.findById(423L)).thenReturn(mensaje);
-        MessageService messageService1= new MessageService(mockmessage);
-        Optional resultadoid=messageService1.getMessageById(423L);
-        assertEquals(resultadoid,423L);
+        Message mensaje = new Message(423L, "Hola");
+        when(mockmessage.findById(423L)).thenReturn(Optional.of(mensaje));
 
+        MessageService messageService = new MessageService(mockmessage);
+        Optional<Message> resultadoId = messageService.getMessageById(423L);
+        assertEquals(Optional.of(mensaje), resultadoId);
     }
-
 }
